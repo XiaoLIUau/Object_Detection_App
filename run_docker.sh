ff9a9fd43582487d3859f0d82f39b900dd6651ba
Xiao LIU <xiao.liu.contact@gmail.com>
2023-12-15 14:26:40 +1000

    add device pass into vm on linux machine

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,9 @@
 source ./set_env.sh
 
 # Run the Docker container with environment variables
-docker run -e EMAIL_USER="$EMAIL_USER" -e EMAIL_PASSWORD="$EMAIL_PASSWORD" -p 5000:5000 my_app_image
+docker run 
+--device=/dev/video0
+-e EMAIL_USER="$EMAIL_USER" 
+-e EMAIL_PASSWORD="$EMAIL_PASSWORD" 
+-p 5000:5000 
+my_app_image
