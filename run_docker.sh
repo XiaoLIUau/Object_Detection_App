40a02fc73a1e62fb906f74930626b2c01ec8b747
Xiao LIU <xiao.liu.contact@gmail.com>
2023-12-15 14:17:28 +1000

    update docker image name

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@
 source ./set_env.sh
 
 # Run the Docker container with environment variables
-docker run -e EMAIL_USER="$EMAIL_USER" -e EMAIL_PASSWORD="$EMAIL_PASSWORD" -p 5000:5000 your_container_name
+docker run -e EMAIL_USER="$EMAIL_USER" -e EMAIL_PASSWORD="$EMAIL_PASSWORD" -p 5000:5000 my_app_image
